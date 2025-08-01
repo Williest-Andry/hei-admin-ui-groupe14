93b1245a8d6680630174c5461468725d02e60291
kenny-antonio <hei.kenny.4@gmail.com>
2025-08-01 14:51:22 +0300

    fix: refact file teacher attendance

--- cypress/e2e/teacher.attendance.cy.tsx
+++ cypress/e2e/teacher.attendance.cy.tsx
@@ -1,25 +1,12 @@
-describe("teacher attendance real test", () => {
-    before("teacher can list & change status event participant real test", () => {
-      cy.visit("/login");
-      cy.getByTestid("casdoor-login-btn").click();
-      cy.origin(Cypress.env("REACT_APP_CASDOOR_SDK_SERVER_URL"), () => {
-        cy.get(
-          "input[placeholder='identifiant, adresse e-mail ou téléphone']"
-        ).type(Cypress.env("REACT_APP_TEST_TEACHER1_EMAIL"));
-        cy.get("input[placeholder='Mot de passe']").type(
-          Cypress.env("REACT_APP_TEST_TEACHER1_PASSWORD")
-        );
-        cy.get("button[type='submit']").click();
-      });
-      cy.on("fail", (err) => {
-        if (err.message.includes("Timed out retrying")) {
-          cy.go("back");
-          cy.getByTestid("casdoor-login-btn").click();
-          return false;
-        }
-        throw err;
-      });
-    })
+describe("Teacher Event Participation Management - Real Data Test", () => {
+  it("teacher can list & change status event participant", () => {
+    cy.visit("/login");
+    cy.getByTestid("casdoor-login-btn").click();
+    cy.origin(Cypress.env("REACT_APP_CASDOOR_SDK_SERVER_URL"), () => {
+      cy.get("input[placeholder='identifiant, adresse e-mail ou téléphone']").type(Cypress.env("REACT_APP_TEST_TEACHER1_EMAIL"));
+      cy.get("input[placeholder='Mot de passe']").type(Cypress.env("REACT_APP_TEST_TEACHER1_PASSWORD"));
+      cy.get("button[type='submit']").click();
+    });
     cy.url().should("include", "/");
     cy.get('[data-testid="event-menu"]').click();
     cy.contains("Listes").click();
@@ -54,4 +41,5 @@ describe("teacher attendance real test", () => {
       });
     cy.contains("Sauvegarder").click();
     cy.contains("Enregistrer avec succès.");
+  });
 });
