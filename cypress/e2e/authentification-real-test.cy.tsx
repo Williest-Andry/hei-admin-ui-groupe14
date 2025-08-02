ba0f153ce80b01356fb4b7c32fef7a25be7b5cac
Niavo <154576396+niavo05@users.noreply.github.com>
2025-08-02 12:21:44 +0300

    change a syntax error authentification test

--- cypress/e2e/authentification-real-test.cy.tsx
+++ cypress/e2e/authentification-real-test.cy.tsx
@@ -3,7 +3,7 @@ describe("Authentification réelle via Casdoor", () => {
   it("should authenticate the STUDENT and land on profile page", () => {
 
     cy.visit("/login", { timeout: 10000 });
-    cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+    cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
     cy.origin("https://numer.casdoor.com", {args: {email: Cypress.env("REACT_APP_TEST_STUDENT1_EMAIL"),password: Cypress.env("REACT_APP_TEST_STUDENT1_PASSWORD")}}, ({ email, password }) => {
       cy.get("input[placeholder='identifiant, adresse e-mail ou téléphone']", { timeout: 10000 }).type(email);
       cy.get("input[placeholder='Mot de passe']", { timeout: 10000 }).type(password);
@@ -12,7 +12,7 @@ describe("Authentification réelle via Casdoor", () => {
     cy.on("fail", (err) => {
       if (err.message.includes("Timed out retrying")) {
         cy.go("back");
-        cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+        cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
         return false;
       } else if (
         err.message.includes(
@@ -21,28 +21,28 @@ describe("Authentification réelle via Casdoor", () => {
         err.message.includes("but the application is at origin")
       ) {
         cy.visit(Cypress.env("REACT_PREPROD_URL") + "/login", { timeout: 10000 });
-        cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+        cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
         return false;
       }
       throw err;
     });
 
 
-    cy.get('[data-testid]="AccountCircleIcon"', { timeout: 10000 }).click();
-    cy.get('[data-testid]="main-content"', { timeout: 10000 }).should("exist");
+    cy.get("[data-testid]='AccountCircleIcon'", { timeout: 10000 }).click();
+    cy.get("[data-testid]='main-content'", { timeout: 10000 }).should("exist");
     cy.get("body", { timeout: 10000 }).should("contain", "STD21001")
       .and("contain", "Ryan")
       .and("contain", "Andria")
       .and("contain", "test+ryan@hei.school")
       .and("contain", "0322411123");
-    cy.get('[data-testid]="LogoutIcon"', { timeout: 10000 }).click();
+    cy.get("[data-testid]='LogoutIcon'", { timeout: 10000 }).click();
   });
 
 
   it("should authenticate the TEACHER and land on profile page", () => {
 
     cy.visit("/login", { timeout: 10000 });
-    cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+    cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
     cy.origin("https://numer.casdoor.com", {args: {email: Cypress.env("REACT_APP_TEST_TEACHER1_EMAIL"),password: Cypress.env("REACT_APP_TEST_TEACHER1_PASSWORD")}}, ({ email, password }) => {
       cy.get("input[placeholder='identifiant, adresse e-mail ou téléphone']", { timeout: 10000 }).type(email);
       cy.get("input[placeholder='Mot de passe']", { timeout: 10000 }).type(password);
@@ -51,7 +51,7 @@ describe("Authentification réelle via Casdoor", () => {
     cy.on("fail", (err) => {
       if (err.message.includes("Timed out retrying")) {
         cy.go("back");
-        cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+        cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
         return false;
       } else if (
         err.message.includes(
@@ -60,27 +60,27 @@ describe("Authentification réelle via Casdoor", () => {
         err.message.includes("but the application is at origin")
       ) {
         cy.visit(Cypress.env("REACT_PREPROD_URL") + "/login", { timeout: 10000 });
-        cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+        cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
         return false;
       }
       throw err;
     });
 
-    cy.get('[data-testid]="AccountCircleIcon"', { timeout: 10000 }).click();
-    cy.get('[data-testid]="main-content"', { timeout: 10000 }).should("exist");
+    cy.get("[data-testid]='AccountCircleIcon'", { timeout: 10000 }).click();
+    cy.get("[data-testid]='main-content'", { timeout: 10000 }).should("exist");
     cy.get("body", { timeout: 10000 }).should("contain", "TCR21001")
       .and("contain", "One")
       .and("contain", "Teacher")
       .and("contain", "test+teacher1@hei.school")
       .and("contain", "0322411125");
-    cy.get('[data-testid]="LogoutIcon"', { timeout: 10000 }).click();
+    cy.get("[data-testid]='LogoutIcon'", { timeout: 10000 }).click();
   });
 
 
   it("should authenticate the MANAGER and land on profile page", () => {
 
     cy.visit("/login", { timeout: 10000 });
-    cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+    cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
     cy.origin("https://numer.casdoor.com", {args: {email: Cypress.env("REACT_APP_TEST_MANAGER1_EMAIL"),password: Cypress.env("REACT_APP_TEST_MANAGER1_PASSWORD")}}, ({ email, password }) => {
       cy.get("input[placeholder='identifiant, adresse e-mail ou téléphone']", { timeout: 10000 }).type(email);
       cy.get("input[placeholder='Mot de passe']", { timeout: 10000 }).type(password);
@@ -89,7 +89,7 @@ describe("Authentification réelle via Casdoor", () => {
     cy.on("fail", (err) => {
       if (err.message.includes("Timed out retrying")) {
         cy.go("back");
-        cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+        cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
         return false;
       } else if (
         err.message.includes(
@@ -98,27 +98,27 @@ describe("Authentification réelle via Casdoor", () => {
         err.message.includes("but the application is at origin")
       ) {
         cy.visit(Cypress.env("REACT_PREPROD_URL") + "/login", { timeout: 10000 });
-        cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+        cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
         return false;
       }
       throw err;
     });
 
-    cy.get('[data-testid]="AccountCircleIcon"', { timeout: 10000 }).click();
-    cy.get('[data-testid]="main-content"', { timeout: 10000 }).should("exist");
+    cy.get("[data-testid]='AccountCircleIcon'", { timeout: 10000 }).click();
+    cy.get("[data-testid]='main-content'", { timeout: 10000 }).should("exist");
     cy.get("body", { timeout: 10000 }).should("contain", "MGR21001")
       .and("contain", "One")
       .and("contain", "Managers")
       .and("contain", "test+manager1@hei.school")
       .and("contain", "0322411124");
-    cy.get('[data-testid]="LogoutIcon"', { timeout: 10000 }).click();
+    cy.get("[data-testid]='LogoutIcon'", { timeout: 10000 }).click();
   });
 
 
   it("should authenticate the ADMIN and land on profile page", () => {
 
     cy.visit("/login", { timeout: 10000 });
-    cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+    cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
     cy.origin("https://numer.casdoor.com", {args: {email: Cypress.env("REACT_APP_TEST_ADMIN1_EMAIL"),password: Cypress.env("REACT_APP_TEST_ADMIN1_PASSWORD")}}, ({ email, password }) => {
       cy.get("input[placeholder='identifiant, adresse e-mail ou téléphone']", { timeout: 10000 }).type(email);
       cy.get("input[placeholder='Mot de passe']", { timeout: 10000 }).type(password);
@@ -127,7 +127,7 @@ describe("Authentification réelle via Casdoor", () => {
     cy.on("fail", (err) => {
       if (err.message.includes("Timed out retrying")) {
         cy.go("back");
-        cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+        cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
         return false;
       } else if (
         err.message.includes(
@@ -136,19 +136,19 @@ describe("Authentification réelle via Casdoor", () => {
         err.message.includes("but the application is at origin")
       ) {
         cy.visit(Cypress.env("REACT_PREPROD_URL") + "/login", { timeout: 10000 });
-        cy.get('[data-testid]="casdoor-login-btn"', { timeout: 10000 }).click();
+        cy.get("[data-testid]='casdoor-login-btn'", { timeout: 10000 }).click();
         return false;
       }
       throw err;
     });
 
-    cy.get('[data-testid]="AccountCircleIcon"', { timeout: 10000 }).click();
-    cy.get('[data-testid]="main-content"', { timeout: 10000 }).should("exist");
+    cy.get("[data-testid]='AccountCircleIcon'", { timeout: 10000 }).click();
+    cy.get("[data-testid]='main-content'", { timeout: 10000 }).should("exist");
     cy.get("body", { timeout: 10000 }).should("contain", "ADM21001")
       .and("contain", "Admin")
       .and("contain", "Admin")
       .and("contain", "test+admin@hei.school")
       .and("contain", "0322411128");
-    cy.get('[data-testid]="LogoutIcon"', { timeout: 10000 }).click();
+    cy.get("[data-testid]='LogoutIcon'", { timeout: 10000 }).click();
   });
 });
