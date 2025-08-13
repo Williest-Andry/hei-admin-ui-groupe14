7de3cd862cd103a1d9b094edc0699cd8c5d237f1
KareemNomenjanahary12 <hei.nomena.2@gmail.com>
2025-08-13 09:20:16 +0300

    fix: Adaptation of import.student with faker.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+import { generateStudentRef } from "./cypress/support/tasks.ts";
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        generateStudentRef
+      });
+      return config;
     },
     defaultCommandTimeout: 20000,
     supportFile: "cypress/support/e2e.ts",
@@ -11,40 +18,24 @@ export default defineConfig({
     experimentalOriginDependencies: true
   },
   env: {
-    REACT_APP_TEST_STUDENT1_PASSWORD:
-      process.env.REACT_APP_TEST_STUDENT1_PASSWORD,
-    REACT_APP_TEST_STUDENT1_EMAIL:
-      process.env.REACT_APP_TEST_STUDENT1_EMAIL,
-    REACT_APP_TEST_TEACHER1_PASSWORD:
-      process.env.REACT_APP_TEST_TEACHER1_PASSWORD,
-    REACT_APP_TEST_TEACHER1_EMAIL:
-      process.env.REACT_APP_TEST_TEACHER1_EMAIL,
-    REACT_APP_TEST_MANAGER1_PASSWORD:
-      process.env.REACT_APP_TEST_MANAGER1_PASSWORD,
-    REACT_APP_TEST_MANAGER1_EMAIL:
-      process.env.REACT_APP_TEST_MANAGER1_EMAIL,
-    REACT_APP_TEST_MONITOR1_PASSWORD:
-      process.env.REACT_APP_TEST_MONITOR1_PASSWORD,
-    REACT_APP_TEST_ADMIN1_PASSWORD:
-      process.env.REACT_APP_TEST_ADMIN1_PASSWORD,
-    REACT_APP_TEST_ADMIN1_EMAIL:
-      process.env.REACT_APP_TEST_ADMIN1_EMAIL,
-    REACT_APP_TEST_STAFF1_PASSWORD:
-      process.env.REACT_APP_TEST_STAFF1_PASSWORD,
-    REACT_APP_TEST_ORGANIZER1_PASSWORD:
-      process.env.REACT_APP_TEST_ORGANIZER1_PASSWORD,
-    REACT_PREPROD_URL:
-      process.env.REACT_PREPROD_URL,
+    REACT_APP_TEST_STUDENT1_PASSWORD: process.env.REACT_APP_TEST_STUDENT1_PASSWORD,
+    REACT_APP_TEST_STUDENT1_EMAIL: process.env.REACT_APP_TEST_STUDENT1_EMAIL,
+    REACT_APP_TEST_TEACHER1_PASSWORD: process.env.REACT_APP_TEST_TEACHER1_PASSWORD,
+    REACT_APP_TEST_TEACHER1_EMAIL: process.env.REACT_APP_TEST_TEACHER1_EMAIL,
+    REACT_APP_TEST_MANAGER1_PASSWORD: process.env.REACT_APP_TEST_MANAGER1_PASSWORD,
+    REACT_APP_TEST_MANAGER1_EMAIL: process.env.REACT_APP_TEST_MANAGER1_EMAIL,
+    REACT_APP_TEST_MONITOR1_PASSWORD: process.env.REACT_APP_TEST_MONITOR1_PASSWORD,
+    REACT_APP_TEST_ADMIN1_PASSWORD: process.env.REACT_APP_TEST_ADMIN1_PASSWORD,
+    REACT_APP_TEST_ADMIN1_EMAIL: process.env.REACT_APP_TEST_ADMIN1_EMAIL,
+    REACT_APP_TEST_STAFF1_PASSWORD: process.env.REACT_APP_TEST_STAFF1_PASSWORD,
+    REACT_APP_TEST_ORGANIZER1_PASSWORD: process.env.REACT_APP_TEST_ORGANIZER1_PASSWORD,
+    REACT_PREPROD_URL: process.env.REACT_PREPROD_URL,
     codeCoverage: {
       exclude: ["cypress/**/*.*", "src/**/*.cy", "src/providers/**/*.*"],
     },
-    REACT_APP_CASDOOR_SDK_SERVER_URL:
-      process.env.REACT_APP_CASDOOR_SDK_SERVER_URL,
-    REACT_APP_TEST_MONITOR1_EMAIL:
-      process.env.REACT_APP_TEST_MONITOR1_EMAIL,
-    INSTATUS_WEBHOOK_URL:
-      process.env.INSTATUS_WEBHOOK_URL,
-    INSTATUS_API_KEY:
-      process.env.INSTATUS_API_KEY,
+    REACT_APP_CASDOOR_SDK_SERVER_URL: process.env.REACT_APP_CASDOOR_SDK_SERVER_URL,
+    REACT_APP_TEST_MONITOR1_EMAIL: process.env.REACT_APP_TEST_MONITOR1_EMAIL,
+    INSTATUS_WEBHOOK_URL: process.env.INSTATUS_WEBHOOK_URL,
+    INSTATUS_API_KEY: process.env.INSTATUS_API_KEY,
   },
 });
--- cypress/e2e/import.students.cy.tsx
+++ cypress/e2e/import.students.cy.tsx
@@ -1,5 +1,33 @@
-describe("Importation d'étudiants", () => {
+
+ import { faker } from '@faker-js/faker';
+
+ describe("Importation d'étudiants", () => {
     it("Redirige vers Casdoor, se connecte avec un compte manager et arrive sur le dashboard", () => {
+        const gender = faker.person.sexType();
+        const firstName = faker.person.firstName(gender);
+        const lastName = faker.person.lastName();
+        const prefixes = ["034", "038", "033", "032", "037"];
+        const prefix = faker.helpers.arrayElement(prefixes);
+        const number = prefix + faker.string.numeric(7);
+        const latitude = faker.location.latitude({ min: -25.6, max: -11.5 });
+        const longitude = faker.location.longitude({ min: 43.2, max: 50.5 });
+        const nic = faker.string.numeric(12)
+        const birthPlace = faker.location.city();
+        const birthDate = faker.date.between({
+            from: new Date('1990-01-01'),
+            to: new Date('2010-12-31')
+        });
+        const formattedBirthDate = birthDate.toISOString().split('T')[0];
+        const address = faker.location.streetAddress() + ', ' + faker.location.city();
+        const email = `${firstName}.${lastName}@gmail.com`;
+        const entranceDate = faker.date.between({
+            from: new Date('2021-01-01'),
+            to: new Date('2024-12-31')
+        });
+        const formattedEntranceDate = entranceDate.toISOString().split('T')[0];
+        const highSchoolOrigin =  faker.company.name()
+
+
         cy.visit("/login");
 
         cy.get('[data-testid="casdoor-login-btn"]').click();
@@ -24,21 +52,31 @@ describe("Importation d'étudiants", () => {
         cy.get('svg[data-testid="SchoolIcon"]').click();
         cy.get('svg[data-testid="PeopleIcon"]').click();
         cy.get('svg[data-testid="AddIcon"]').click();
-        cy.get('input[id="ref"]').type("ETD004_groupe14");
-        cy.get('input[id="first_name"]').type("kareemmm_groupe14");
-        cy.get('input[id="last_name"]').type("Nomenjanaharyyyyyyyy_groupe14");
-        cy.get('input[id="sex_M"]').click();
-        cy.get('input[id="phone"]').type("0345368840");
-        cy.get('input[id="coordinates.latitude"]').type("1");
-        cy.get('input[id="coordinates.longitude"]').type("1");
-        cy.get('input[id="nic"]').type("126456789123");
-        cy.get('input[id="birth_place"]').type("Soavinandriana");
-        cy.get('input[id="birth_date"]').type("2003-06-03");
-        cy.get('textarea[id="address"]').type("Caspat soanierana");
-        cy.get('input[id="email"]').type("hei.nomna.2@gmail.com");
-        cy.get('input[id="entrance_datetime"]').type("2022-06-05");
-        cy.get('input[id="high_school_origin"]').type("lycee moderne ampefilou");
+        cy.task("generateStudentRef").then((ref) => {
+            const studentRef = ref as string;
+            cy.log(`ID généré : ${studentRef}`);
+            cy.get('input[id="ref"]').type(studentRef);
+        });
+        cy.get('input[id="first_name"]').type(firstName);
+        cy.get('input[id="last_name"]').type(lastName);
+        if (gender === 'female') {
+            cy.get('input[id="sex_F"]').check({ force: true });
+        } else {
+            cy.get('input[id="sex_M"]').check({ force: true });
+        }
+        cy.get('input[id="phone"]').type(number);
+        cy.get('input[id="coordinates.latitude"]').type(latitude.toString());
+        cy.get('input[id="coordinates.longitude"]').type(longitude.toString());
+        cy.get('input[id="nic"]').type(nic);
+        cy.get('input[id="birth_place"]').type(birthPlace);
+        cy.get('input[id="birth_date"]').type(formattedBirthDate);
+        cy.get('textarea[id="address"]').type(address);
+        cy.get('input[id="email"]').type(email);
+        cy.get('input[id="entrance_datetime"]').type(formattedEntranceDate);
+        cy.get('input[id="high_school_origin"]').type(highSchoolOrigin);
         cy.get('button[type="submit"]').click();
         cy.get('svg[data-testid="PeopleIcon"]').click();
+
+
     });
 });
